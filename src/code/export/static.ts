3e5de250c921a3602831049a953f6f185f7e1a5b
Luciism <lucism.dev@gmail.com>
2026-01-30 04:56:54 +1300

    feat: add mask exporting

--- src/code/export/static.ts
+++ src/code/export/static.ts
@@ -1,155 +1,193 @@
-import { ContentBoxSchema, RasterSizeSchema, StaticBaseSchema } from "../../shared/schema-types.ts";
+import {
+  ContentBoxSchema,
+  RasterSizeSchema,
+  StaticBaseSchema,
+} from "../../shared/schema-types.ts";
 import { isStyleableNode } from "../nodes.ts";
 import getTaggedNodes from "../tagging.ts";
 import { changeNodeFillOpacity } from "./color.ts";
 
 /** Requires rasterizeOpaque() to be called beforehand. */
 async function rasterizeTranslucent(clone: FrameNode) {
-    // Recursively traverses frames to find the first one with a fill.
-    // This sets the opacity of each tile
-    const recursivelyUnfillBaseFrame = (node: FrameNode) => {
-        node.children.forEach(child => {
-            if (child.type == "INSTANCE") {
-                child = child.detachInstance();
-            }
+  // Recursively traverses frames to find the first one with a fill.
+  // This sets the opacity of each tile
+  const recursivelyUnfillBaseFrame = (node: FrameNode) => {
+    node.children.forEach((child) => {
+      if (child.type == "INSTANCE") {
+        child = child.detachInstance();
+      }
 
-            if (child.type == "FRAME" ) {
-                // Ensure it isn't a layout frame
-                if (typeof child.fills != "symbol" && child.fills.length) {
-                    changeNodeFillOpacity(child, 0.5);
-                    return;
-                }
+      if (child.type == "FRAME") {
+        // Ensure it isn't a layout frame
+        if (typeof child.fills != "symbol" && child.fills.length) {
+          changeNodeFillOpacity(child, 0.8);
+          return;
+        }
 
-                recursivelyUnfillBaseFrame(child);
-            }
-        })
+        recursivelyUnfillBaseFrame(child);
+      }
+    });
+  };
+  recursivelyUnfillBaseFrame(clone);
 
-    }
-    recursivelyUnfillBaseFrame(clone);
+  const filename = `${clone.name}-static-translucent.png`
+    .replace(/\s+/g, "-")
+    .toLowerCase();
+  const file = await clone.exportAsync({ format: "PNG" });
 
-    const filename = `${clone.name}-static-translucent.png`.replace(/\s+/g, "-").toLowerCase();
-    const file = await clone.exportAsync({format: "PNG"})
-
-    return {filename, file}
+  return { filename, file };
 }
 
 async function rasterizeOpaque(clone: FrameNode) {
-    const taggedNodes = getTaggedNodes([clone]);
+  const taggedNodes = getTaggedNodes([clone]);
 
-    taggedNodes.forEach(taggedNode => {
-        if (isStyleableNode(taggedNode)) {
-            taggedNode.opacity = 0;
-        }
-    });
+  taggedNodes.forEach((taggedNode) => {
+    if (isStyleableNode(taggedNode)) {
+      taggedNode.opacity = 0;
+    }
+  });
 
-    const filename = `${clone.name}-static.png`.replace(/\s+/g, "-").toLowerCase();
-    const file = await clone.exportAsync({format: "PNG"})
+  const filename = `${clone.name}-static.png`
+    .replace(/\s+/g, "-")
+    .toLowerCase();
+  const file = await clone.exportAsync({ format: "PNG" });
 
-    return {filename, file};
+  return { filename, file };
 }
-
 
 async function rasterizeMask(clone: FrameNode) {
-    clone.x = 0;
-    clone.y = 10000;
+  clone.x = 0;
+  clone.y = 10000;
 
-    const recursivelyHideContent = (node: FrameNode | InstanceNode) => {
-        node.layoutSizingVertical = "FIXED";
-        node.layoutSizingHorizontal = "FIXED";
+  const bounds = clone.absoluteRenderBounds;
+  const [absoluteWidth, absoluteHeight] = bounds
+    ? [bounds.width, bounds.height]
+    : [clone.width, clone.height];
 
-        node.children.forEach(child => {
-            if (child.type == "INSTANCE") {
-                child = child.detachInstance();
-            }
+  const recursivelyHideContent = (node: FrameNode | InstanceNode) => {
+    node.layoutSizingVertical = "FIXED";
+    node.layoutSizingHorizontal = "FIXED";
 
-            if (child.type != "FRAME") {
-                child.remove();
-            } else {
-                recursivelyHideContent(child);
-            }
-        })
+    node.children.forEach((child) => {
+      if (child.type == "INSTANCE") {
+        child = child.detachInstance();
+      }
+
+      if (child.type != "FRAME") {
+        child.remove();
+      } else {
+        recursivelyHideContent(child);
+      }
+    });
+  };
+
+  recursivelyHideContent(clone);
+
+  const recursivelyRemoveEffects = (node: FrameNode) => {
+    node.effects = [];
+    node.strokes = [];
+
+    if (typeof node.fills == "symbol" || node.fills.length) {
+      node.fills = [figma.util.solidPaint("#FFFFFF")];
     }
 
-    recursivelyHideContent(clone);
+    node.children.forEach((child) => {
+      if (child.type == "FRAME") {
+        recursivelyRemoveEffects(child);
+      }
+    });
+  };
 
-    const recursivelyRemoveEffects = (node: FrameNode) => {
-        node.effects = [];
-        node.strokes = [];
+  recursivelyRemoveEffects(clone);
+  clone.fills = [figma.util.solidPaint("#000000")];
 
-        if (typeof node.fills == "symbol" || node.fills.length) {
-            node.fills = [figma.util.solidPaint("#000000")];
-        }
+  // Ensure offset due to effects are accounted for.
+  if (absoluteWidth > clone.width) {
+    const offset = Math.floor((absoluteWidth - clone.width) / 2);
+    clone.paddingLeft += offset;
+    clone.paddingRight += offset;
+  }
 
-        node.children.forEach(child => {
-            if (child.type == "FRAME") {
-                recursivelyRemoveEffects(child);
-            }
-        });
-    }
+  if (absoluteHeight > clone.height) {
+    const offset = Math.floor((absoluteHeight - clone.height) / 2);
+    clone.paddingTop += offset;
+    clone.paddingBottom += offset;
+  }
 
-    recursivelyRemoveEffects(clone);
+    clone.resize(absoluteWidth, absoluteHeight);
+    clone.primaryAxisAlignItems = "CENTER";
+    clone.counterAxisAlignItems = "CENTER";
 
-    const filename = `${clone.name}-mask.png`.replace(/\s+/g, "-").toLowerCase();
-    const file = await clone.exportAsync({format: "PNG"})
 
-    clone.remove();
+  const filename = `${clone.name}-mask.png`.replace(/\s+/g, "-").toLowerCase();
+  const file = await clone.exportAsync({ format: "PNG" });
 
-    return {filename, file};
+  clone.remove();
+
+  return { filename, file };
 }
 
+export default async function exportRasterizedStaticElements(
+  node: SceneNode,
+): Promise<{
+  schemas: {
+    name: string;
+    contentBox: ContentBoxSchema;
+    rasterSize: RasterSizeSchema;
+    staticBase: StaticBaseSchema;
+  };
+  files: { filename: string; file: string | Uint8Array }[];
+}> {
+  if (node.type != "FRAME") {
+    throw new Error("Master node must be a frame.");
+  }
 
-export default async function exportRasterizedStaticElements(node: SceneNode): Promise<{schemas: {name: string, contentBox: ContentBoxSchema, rasterSize: RasterSizeSchema, staticBase: StaticBaseSchema}, files: {filename: string, file: string | Uint8Array}[]}> {
-    if (node.type != "FRAME") {
-        throw new Error("Master node must be a frame.");
-    }
+  const clone = node.clone();
 
-    const clone = node.clone();
-    
-    let rasterSize: RasterSizeSchema = {
-        width: clone.width,
-        height: clone.height
+  let rasterSize: RasterSizeSchema = {
+    width: Math.round(clone.width),
+    height: Math.round(clone.height),
+  };
+  const contentBox: ContentBoxSchema = {
+    width: Math.round(clone.width),
+    height: Math.round(clone.height),
+    rasterX: 0,
+    rasterY: 0,
+  };
+
+  const box = clone.absoluteRenderBounds;
+
+  if (box) {
+    rasterSize = {
+      width: Math.round(box.width),
+      height: Math.round(box.height),
     };
-    const contentBox: ContentBoxSchema = {
-        width: clone.width,
-        height: clone.height,
-        rasterX: 0,
-        rasterY: 0
-    }
+    contentBox.rasterX = Math.round(clone.x - box.x);
+    contentBox.rasterY = Math.round(clone.y - box.y);
+  }
 
-    const box = clone.absoluteRenderBounds;
+  clone.x = 0;
+  clone.y = 10000;
 
-    if (box) {
-        rasterSize = {
-            width: box.width,
-            height: box.height
-        };
-        contentBox.rasterX = clone.x - box.x;
-        contentBox.rasterY = clone.y - box.y;
+  const opaque = await rasterizeOpaque(clone);
+  const translucent = await rasterizeTranslucent(clone);
+  const mask = await rasterizeMask(node.clone());
 
-    }
+  clone.remove();
 
-    clone.x = 0;
-    clone.y = 10000;
+  const files = [opaque, translucent, mask];
 
-    const opaque = await rasterizeOpaque(clone);
-    const translucent = await rasterizeTranslucent(clone);
-    const mask = await rasterizeMask(node.clone());
-
-    clone.remove();
-
-    const files = [opaque, translucent, mask];
-
-    return {
-        schemas: {
-            name: node.name,
-            contentBox,
-            rasterSize,
-            staticBase: {
-                opaque: opaque.filename + ".png",
-                translucent: translucent.filename + ".png",
-                mask: mask.filename + ".png"
-            }
-        },
-        files
-    }
+  return {
+    schemas: {
+      name: node.name,
+      contentBox,
+      rasterSize,
+      staticBase: {
+        opaque: opaque.filename,
+        translucent: translucent.filename,
+        mask: mask.filename,
+      },
+    },
+    files,
+  };
 }
