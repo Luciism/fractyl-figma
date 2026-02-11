6dbaedd7897af90a2df555c7a52cde9968214d4e
Luciism <lucism.dev@gmail.com>
2026-02-11 15:35:39 +1300

    fix: use parent box position in schema when clipping to parent

--- src/code/export/dynamic.ts
+++ src/code/export/dynamic.ts
@@ -1,55 +1,101 @@
-import { ImageFragmentSchema, ShapeFragmentSchema, TextFragmentSchema } from "../../shared/schema-types.ts";
+import {
+    ImageFragmentSchema,
+    ShapeFragmentSchema,
+    TextFragmentSchema,
+} from "../../shared/schema-types.ts";
 import { NodePluginData } from "../../shared/types.ts";
 import { getNodeId } from "../ids.ts";
-import { getColorMode, getShapeHeightMode, getShapeWidthMode } from "../modes.ts";
+import {
+    getColorMode,
+    getShapeHeightMode,
+    getShapeWidthMode,
+} from "../modes.ts";
 import getTaggedNodes from "../tagging.ts";
 import { recursivelyRemoveStaticColors } from "./color.ts";
 import exportImageFragments from "./dynamic/images.ts";
+import { getShouldClipToParent } from "./dynamic/shapes/clipping.ts";
 import { rectangleToSVG } from "./dynamic/shapes/rect.ts";
 import exportTextFragments from "./dynamic/text.ts";
 
 function exportShapes(masterNode: SceneNode, taggedNodes: SceneNode[]) {
-    const shapeFragmentSvgs: {svgCode: string, pluginData: NodePluginData, schema: ShapeFragmentSchema}[] = [];
+    const shapeFragmentSvgs: {
+        svgCode: string;
+        pluginData: NodePluginData;
+        schema: ShapeFragmentSchema;
+    }[] = [];
 
-    taggedNodes.filter(node => node.type == "RECTANGLE").forEach(node => {
-        if (typeof node.fills != "symbol" && node.fills[0] && node.fills[0].type != "IMAGE") {
-            const fragment = rectangleToSVG(node);
+    taggedNodes
+        .filter((node) => node.type == "RECTANGLE")
+        .forEach((node) => {
+            if (
+                typeof node.fills != "symbol" &&
+                node.fills[0] &&
+                node.fills[0].type != "IMAGE"
+            ) {
+                const fragment = rectangleToSVG(node);
 
-            let [x, y]: [number, number] = [0, 0];
-            const box = node.absoluteBoundingBox;
+                let [x, y]: [number, number] = [0, 0];
+                let box;
 
-            if (box) {
-                x = Math.round(box.x - masterNode.x);
-                y = Math.round(box.y - masterNode.y);
+                const parent = node.parent;
+                const shouldClip = getShouldClipToParent(node);
+
+                // Use parent box if the node should clip to parent
+                if (
+                    parent &&
+                    (parent.type == "FRAME" || parent.type == "INSTANCE") &&
+                    (shouldClip || shouldClip == null)
+                ) {
+                    box = parent.absoluteBoundingBox;
+                } else {
+                    box = node.absoluteBoundingBox;
+                }
+
+                if (box) {
+                    x = Math.round(box.x - masterNode.x);
+                    y = Math.round(box.y - masterNode.y);
+                }
+
+                const id = getNodeId(node) || Math.round(Math.random() * 10000);
+                const schema = {
+                    src: `shapes/rect-${id}.svg`,
+                    widthMode: getShapeWidthMode(node),
+                    heightMode: getShapeHeightMode(node),
+                    colorMode: getColorMode(node),
+                    position: { x, y },
+                    placeholders: fragment.placeholders,
+                };
+
+                shapeFragmentSvgs.push({ ...fragment, schema });
             }
-
-            const id = getNodeId(node) || Math.round(Math.random() * 10000);
-            const schema = {
-                src: `shapes/rect-${id}.svg`,
-                widthMode: getShapeWidthMode(node),
-                heightMode: getShapeHeightMode(node),
-                colorMode: getColorMode(node),
-                position: {x, y},
-                placeholders: fragment.placeholders
-            };
-
-            shapeFragmentSvgs.push({...fragment, schema});
-        }
-    });
+        });
 
     return shapeFragmentSvgs;
 }
 
 export default async function exportDynamicFragments(
     node: SceneNode,
-): Promise<{schemas: {textFragments: TextFragmentSchema[], imageFragments: ImageFragmentSchema[], shapeFragments: ShapeFragmentSchema[]}, files: {filename: string, type: string, file: string | Uint8Array}[]}> {
+): Promise<{
+    schemas: {
+        textFragments: TextFragmentSchema[];
+        imageFragments: ImageFragmentSchema[];
+        shapeFragments: ShapeFragmentSchema[];
+    };
+    files: { filename: string; type: string; file: string | Uint8Array }[];
+}> {
     const clone = node.clone();
 
     const taggedNodes = getTaggedNodes([clone]);
     recursivelyRemoveStaticColors(taggedNodes, clone);
 
-    const textFragments = exportTextFragments(clone, taggedNodes.filter(node => node.type == "TEXT"));
-    const imageFragments = exportImageFragments(clone, taggedNodes.filter(node => node.type == "RECTANGLE"));
+    const textFragments = exportTextFragments(
+        clone,
+        taggedNodes.filter((node) => node.type == "TEXT"),
+    );
+    const imageFragments = exportImageFragments(
+        clone,
+        taggedNodes.filter((node) => node.type == "RECTANGLE"),
+    );
 
     const shapeFragments = exportShapes(clone, taggedNodes);
 
@@ -60,31 +106,33 @@ export default async function exportDynamicFragments(
             filename: textFragments.schema.src,
             type: "image/svg",
             file: textFragments.svgCode,
-        }
-    ].concat(
-        shapeFragments.map(fragment => {
-            return {
-                filename: fragment.schema.src,
-                type: "image/svg",
-                file: fragment.svgCode,
-            }
-        })
-    ).concat(
-        imageFragments.map(fragment => {
-            return {
-                filename: fragment.schema.src,
-                type: "image/svg",
-                file: fragment.svgCode,
-            }
-        })
-    );
+        },
+    ]
+        .concat(
+            shapeFragments.map((fragment) => {
+                return {
+                    filename: fragment.schema.src,
+                    type: "image/svg",
+                    file: fragment.svgCode,
+                };
+            }),
+        )
+        .concat(
+            imageFragments.map((fragment) => {
+                return {
+                    filename: fragment.schema.src,
+                    type: "image/svg",
+                    file: fragment.svgCode,
+                };
+            }),
+        );
 
     return {
         schemas: {
             textFragments: [textFragments.schema],
-            imageFragments: imageFragments.map(frag => frag.schema),
-            shapeFragments: shapeFragments.map(frag => frag.schema)
+            imageFragments: imageFragments.map((frag) => frag.schema),
+            shapeFragments: shapeFragments.map((frag) => frag.schema),
         },
-        files
+        files,
     };
 }
