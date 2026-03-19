5280d46f64c3a80735303b4245d026bbd065772b
Luciism <lucism.dev@gmail.com>
2026-03-20 04:41:07 +1300

    fix: filter out irrelevant scales when exporting

--- src/code/export/all.ts
+++ src/code/export/all.ts
@@ -10,26 +10,36 @@ export default async function completeExport(masterNode: SceneNode) {
     const exportSettings = getExportSettings();
     await aggregateExportVariables(exportSettings.includedVariables);
 
-    const layoutSchemas = await Promise.all(exportSettings.scales.map(async (scale, i) => {
-        const fragments = await exportDynamicFragments(masterNode, scale);
-        const statics = await exportRasterizedStaticElements(masterNode, scale);
+    const layoutSchemas = await Promise.all(
+        exportSettings
+            .scales
+            .filter(scale => scale.isEnabled && scale.scale > 0 || scale.isDefault)
+            // Eliminate duplicate scale names
+            .filter((scale, i, scales) => scales.findIndex(s => s.name === scale.name) === i || scale.isDefault)
+            .map(async (scale, i) => {
+                const fragments = await exportDynamicFragments(masterNode, scale);
+                const statics = await exportRasterizedStaticElements(masterNode, scale);
 
-        return {schema: generateLayoutSchema({id: i, scale: {
-            id: scale.id,
-            name: scale.name,
-            isDefault: scale.isDefault,
-            scale: scale.scale
-        },
-            ...statics.schemas,
-            ...fragments.schemas
-        }), files: [...fragments.files, ...statics.files]};
-    }));
+                return {
+                    schema: generateLayoutSchema({
+                        id: i, scale: {
+                            id: scale.id,
+                            name: scale.name,
+                            isDefault: scale.isDefault,
+                            scale: scale.scale
+                        },
+                        ...statics.schemas,
+                        ...fragments.schemas
+                    }), files: [...fragments.files, ...statics.files]
+                };
+            }));
 
     const schema = generateSchema({
         name: masterNode.name,
         layouts: layoutSchemas.map(layout => layout.schema),
-        variables: await aggregateExportVariables(exportSettings.includedVariables)}
-    ); // TODO: variables
+        variables: await aggregateExportVariables(exportSettings.includedVariables)
+    }
+    );
 
     const zip = new JSZip();
     zip.file("schema.json", JSON.stringify(schema))
@@ -41,20 +51,19 @@ export default async function completeExport(masterNode: SceneNode) {
     });
 
     if (typeof setImmediate === "undefined") {
-      // @ts-expect-error setImmediate is not defined in browser typings
-      globalThis.setImmediate = fn => setTimeout(fn, 0);
+        // @ts-expect-error setImmediate is not defined in browser typings
+        globalThis.setImmediate = fn => setTimeout(fn, 0);
     }
 
     let promise = null;
     if (JSZip.support.uint8array) {
-      promise = zip.generateAsync({type : "uint8array"});
+        promise = zip.generateAsync({ type: "uint8array" });
     } else {
-      promise = zip.generateAsync({type : "string"});
+        promise = zip.generateAsync({ type: "string" });
     }
 
     const zipFile = await promise;
 
     const filename = masterNode.name.replace(/\s+/g, "-").toLowerCase();
-    figma.ui.postMessage({type: "complete-export", zipFile, filename});
+    figma.ui.postMessage({ type: "complete-export", zipFile, filename });
 }
-
